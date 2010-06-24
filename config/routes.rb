22c557dafb4cd90b625dd5e032acb91bfa2757cc
Andrew E. Gall <andrew@mashbot.net>
2010-06-24 13:32:31 -0700

    Have a simple Google Map working

--- config/routes.rb
+++ config/routes.rb
@@ -1,5 +1,6 @@
 ActionController::Routing::Routes.draw do |map|
   map.resources :computers
+  map.root :controller => "location", :action => "index"
 
   # The priority is based upon order of creation: first created -> highest priority.
 
